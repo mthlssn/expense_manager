ce897d53dac1dd5c243fdf510e5c826748e79b7b
mthlssn <mthlssn@gmail.com>
2024-07-15 23:38:40 -0300

    update script

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const form_consumo = document.getElementById('form_consumo');
+const form_desconsumo = document.getElementById('form_desconsumo');
+const form_titulo = document.getElementById('form_titulo');
+const form_valor = document.getElementById('form_valor');
+const form_data = document.getElementById('form_data');
+const form_categoria = document.getElementById('form_categoria');
+const form_registar = document.getElementById('form_registar');
+
+
+function criar() {
+    console.log(form_categoria.value + form_consumo.value + form_data.value + form_desconsumo.value + form_registar.value + form_titulo.value +form_valor.value);
+}
